47b2ea165d98c07e22f324fad2ba175007ffa38f
pkaczmarek95 <anicet95@yandex.com>
2017-05-01 15:09:10 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ using namespace std;
 
 	 cap.read(frame);//czytaj pojedyncza klatke
 
-	 double focal_length =frame.cols; // Approximate focal length
+	 double focal_length =frame.cols; // aproksymacja ogniskowej
 	 Point2d center = Point2d(frame.cols/2,frame.rows/2);
 	 Mat camera_matrix = (Mat_<double>(3,3) << focal_length, 0, center.x, 0 , focal_length, center.y, 0, 0, 1);
 	 Mat dist_coeffs =Mat::zeros(4,1,DataType<double>::type); // Assuming no lens distortion
@@ -39,12 +39,54 @@ using namespace std;
 	 Mat imgOriginal_8bit; //obiekt, w którym bedzie przechowana skonwertowana klatka - wymaga tego jedna z funkcji
 
 	 vector<Point2f> corners; //wektor elementow point2f - przechowa wspolrzedne znalezionych krancow pol szachownicy
-	 vector<Point3f> corners_3d; //ten obiekt przyjmuje solvePnP
+
 	 Size patternsize(9,6); //funkcja findchessboardcorners musi znac wielkosc szachownicy - podaje ja tutaj
 
 	 // Output rotation and translation
 	 Mat rotation_vector; // Rotation in axis-angle form
 	 Mat translation_vector;
+
+	 int l_punktow=54;
+
+	 vector<Point3f> corners_3d;
+
+	 //zeby wpisac do wektora, musze najpierw stworzyc 54 puste elementy - tu chyba lezal blad
+
+	 for (int p=0; p<54; p++)
+	 {
+
+	 corners_3d.push_back(Point3d(0,0,0));
+
+	 }
+
+	 float bok_kwad=2.7; //w centymetrach
+
+	 //skrajny lewy dolny punkt niech ma wspolrzedne (0,0,0)
+
+	 corners_3d[0].x=0;
+	 corners_3d[0].y=0;
+	 corners_3d[0].z=0;
+
+	for (int i=1; i<l_punktow; i++)
+	{
+		corners_3d[i].x=corners_3d[i-1].x+bok_kwad;
+
+		if ((i+1)%9==0) // (jak dojedzie do konca wiersza to zeruj x, zwiększ y o wysokosc kwadratu - wypelniamy wyższy wiersz od lewej
+		{
+		corners_3d[i].x=0;
+		corners_3d[i].y=corners_3d[i-1].y+bok_kwad;
+
+		}
+
+		else
+
+		{
+			corners_3d[i].y=corners_3d[i-1].y; //jak nie dojechal, to nie zmieniaj wysokosci y
+		}
+
+		corners_3d[i].z=0; //bo szachownica jest plaska
+
+	}
 
     while (true)
     {
@@ -59,29 +101,11 @@ using namespace std;
         }
 
 
-         imgOriginal.convertTo(imgOriginal_8bit, CV_8U); //konwersja do 8-bitowej glebi - takie obrazy przyjmuje funkcja znajdujaca
-         //cvtColor(imgOriginal_8bit, imgOriginal_8bit, CV_BGR2GRAY); - konwersja na szarosc tez nic nie daje
+         imgOriginal.convertTo(imgOriginal_8bit, CV_8U); //konwersja do 8-bitowej glebi - takie obrazy przyjmuje funkcja znajdujaca findchessboardcorners()
 
          bool patternfound = findChessboardCorners(imgOriginal_8bit, patternsize, corners, CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE + CALIB_CB_FAST_CHECK);
 
          //corners wypelnione znalezionymi wspolrzednymi
-
-         //jak to zostawie, to program wylacza sie, zwracajac -1
-        // Kraft pisal - "Za object points i image points podajcie Panowie punkty na obserwowanej szachownicy 
-	//(znalezione za pomocą findchessboardconers)."
-        //ta funkcja wypluwa tylko wektory punktów o dwoch wspolrzednych <point2f> (czy tam 2d)
-	//wiec tutaj dokladam sobie zero (?) - solvePnP wymaga punktow o trzech wspolrzednych - a jednak nie dziala
-
-         /*for (int i=0; i<corners.size();i++)
-         {
-
-        	 corners_3d[i].x=corners[i].x;
-        	 corners_3d[i].y=corners[i].y;
-        	 corners_3d[i].z=0;
-
-         }*/
-	    
-	 //sprawdzam czy szachownica znaleziona   
 
          if (!patternfound)
          {
@@ -92,23 +116,24 @@ using namespace std;
 
          {
 
-	 //wlasciwe znalezienie translacji i rotacji 
-		 
          solvePnP(corners_3d, corners, camera_matrix, dist_coeffs, rotation_vector, translation_vector);
 
         //wyrzucenie do konsoli
 
-        cout<<rotation_vector<<endl;
+        cout<<"Wektor rotacji"<<rotation_vector<<endl;
 
-        cout<<translation_vector<<endl;
+        cout<<"Wektor translacji"<<translation_vector<<endl;
 
          }
 
-         drawChessboardCorners(imgOriginal_8bit, patternsize, corners, patternfound);
+         drawChessboardCorners(imgOriginal_8bit, patternsize, corners, patternfound); //rysowanie linii
 
-         imshow ("Original", imgOriginal_8bit);
 
-        if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+         imshow ("Original", imgOriginal_8bit);//wyrzucenie do okna
+
+
+
+        if (waitKey(30) == 27) //konczymy program eskejpem
             {
                  cout << "Nacisnieto esc - wyjscie" << endl;
                   break;
